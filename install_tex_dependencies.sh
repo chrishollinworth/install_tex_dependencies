10be557e4e418a6e354553a5dab39ee100337fcc
Chris Hollinworth <chollinworth@gmail.com>
2021-09-16 10:02:30 +0100

    Add script

---
+++ install_tex_dependencies.sh
@@ -0,0 +1,5 @@
+sudo apt install texstudio
+sudo apt install texlive-xetex
+sudo apt-get install texlive-fonts-extra 
+sudo apt-get install texlive-science
+sudo apt-get install texlive-lang-italian
