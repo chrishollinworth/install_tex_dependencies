b438451a74e3c97ed8a26b1aff29fd7f9ed9d188
Chris Hollinworth <chollinworth@gmail.com>
2025-02-17 20:00:05 +0000

    Add Fonts

--- install_tex_dependencies.sh
+++ install_tex_dependencies.sh
@@ -3,3 +3,4 @@ sudo apt install texlive-xetex
 sudo apt-get install texlive-fonts-extra 
 sudo apt-get install texlive-science
 sudo apt-get install texlive-lang-italian
+sudo cp -R fonts/ /usr/share/fonts/opentype/fontin
